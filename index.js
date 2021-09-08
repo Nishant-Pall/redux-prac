909a548766e0420bf93d618cc238fcd3697abad1
Nishant <nishant@loopinhq.com>
2021-09-08 15:12:01 +0530

    Add some comments

--- index.js
+++ index.js
@@ -31,22 +31,25 @@ const initialCakeState = {
 const initialIceCreamState = {
     numOfIceCreams: 20,
 };
-//const reducer = (state = initialState, action) => {
-//switch (action.type) {
-//case BUY_CAKE:
-//return {
-//...state,
-//numOfCakes: state.numOfCakes - 1,
-//};
-//case BUY_ICECREAM:
-//return {
-//...state,
-//numOfIceCreams: state.numOfIceCreams - 1,
-//};
-//default:
-//return state;
-//}
-//};
+
+/*
+ *const reducer = (state = initialState, action) => {
+ *    switch (action.type) {
+ *        case BUY_CAKE:
+ *            return {
+ *                ...state,
+ *                numOfCakes: state.numOfCakes - 1,
+ *            };
+ *        case BUY_ICECREAM:
+ *            return {
+ *                ...state,
+ *                numOfIceCreams: state.numOfIceCreams - 1,
+ *            };
+ *        default:
+ *            return state;
+ *    }
+ *};
+ */
 
 const cakeReducer = (state = initialCakeState, action) => {
     switch (action.type) {
